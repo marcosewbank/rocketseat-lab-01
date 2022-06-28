d0be4ef94da0d4a712d9fc2f7fefe02e2e18ce93
[marcos ewbank] <marcos.suarez@pivotree.com>
2022-06-28 10:53:56 -0300

    finish front-end

---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio")
+  ],
+}
